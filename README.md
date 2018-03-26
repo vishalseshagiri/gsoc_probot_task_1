b1b0af63aeff705563eb8f02505703fbcfd21bf2
Glitch (stellar-coal) <none>
2018-03-26 01:09:42 +0000

    🚌🙎 Updated with Glitch

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
-# gsoc_probot_task_1
-- Probot app that automatically merges pull requests that pass all checks
+# probot-app-merge-pr
+
+[![build status][build-badge]][build-href]
+[![dependencies status][deps-badge]][deps-href]
+[![npm version][npm-badge]][npm-href]
+
+> a GitHub App built with [probot](https://github.com/probot/probot) that merges PRs on command
+
+## Setup
+
+```
+# Install dependencies
+npm install
+
+# Run the bot
+npm start
+```
+
+See [docs/deploy.md](docs/deploy.md) if you would like to run your own instance of this app.
+
+## Config
+
+`.github/merge-pr.yml`
+```yml
+command: "!merge"
+merge_method: squash
+```
+
+[build-badge]: https://badge.buildkite.com/12755c41f29821b7e31405d1e61f6b2fe982a742743e1cc647.svg?branch=master
+[build-href]: https://buildkite.com/uberopensource/probot-app-merge-pr
+[deps-badge]: https://david-dm.org/uber-web/probot-app-merge-pr.svg
+[deps-href]: https://david-dm.org/uber-web/probot-app-merge-pr
+[npm-badge]: https://badge.fury.io/js/probot-app-merge-pr.svg
+[npm-href]: https://www.npmjs.com/package/probot-app-merge-pr
---
+++ index.js
@@ -0,0 +1,63 @@
+/** Copyright (c) 2017 Uber Technologies, Inc.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+var request = require('request')
+
+module.exports = robot => {
+    
+  var number
+  robot.on('pull_request', async context => {
+      console.log(context.payload.number)
+      number = context.payload.number
+    }
+  )
+  
+  robot.on('status', async context => {
+    const isPr = context.payload.context.slice(-2)
+    if(isPr === "pr"){
+      merge(context, number)
+    }
+  }
+  );
+  
+  async function merge(context, number) {
+    const { payload } = context
+    const { github } = context
+    const { sha } = payload
+    const { author } = payload.commit.commit
+    const commit_message = payload.commit.commit.message
+    const commit_title = "Auto commit by probot on successful build"
+    const status = payload.state
+    
+    
+    if(status !== "success"){
+      return
+    }
+  
+    try {
+      console.log(context)
+      await github.pullRequests.merge(
+        context.repo({
+          number:number,
+          commit_title: commit_title,
+          commit_message: commit_message,
+          merge_method: "squash",
+        }),
+      );
+      console.log('got here')
+    } catch (err) {
+      if (err.code === 405) {
+        const message = JSON.parse(err.message).message;
+        github.issues.createComment(
+          context.issue({
+            body: `Failed to merge PR: ${message}`,
+          }),
+        );
+      }
+    }
+    
+  }
+};
